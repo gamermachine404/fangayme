561b88912ac7de9ff332a501042ccbd4c2c669a5
ZeddGoofusIII <zacharyl267@gmail.com>
2024-06-02 17:14:14 -0400

    Co-authored-by: gamermachine404 <gamermachine404@users.noreply.github.com> Co-authored-by: Xamabi <Xamabi@users.noreply.github.co,

--- FanGayme/gaygay.gd
+++ FanGayme/gaygay.gd
@@ -5,8 +5,12 @@ extends TextEdit
 func _ready():
 	pass # Replace with function body.
 
+var cum = 0
+var gyatt = 5000000000
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-#Yipee
+#Yipee 
+# pookie bear 
+      
